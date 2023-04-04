0ee54f9abce8ed83f8d4c7a0ce8c6ae5c5291666
Resplendent-Keklak <107497590+Resplendent-Keklak@users.noreply.github.com>
2023-04-03 20:44:05 -0400

    Create InputForm.py

---
+++ InputForm.py
@@ -0,0 +1,43 @@
+# Data-Sensitive Inputs
+def enter_datasensitive(
+  bool(input("Do you consent to have your device's locational data used for the form? "))
+  bool(input("Do you consent to have the similarity algorithm use the metadata from any file(s) uploaded to the form, which likely contain sensitive information about you and your device?" ))
+  #bool(input("Do you consent to have any additional sensitive data used in the form to benefit your results? "))
+  break
+)
+# Locational Inputs
+def enter_locational(
+  #str(input("Oceanic or found on continent? "))
+  break
+)
+# Datetime Inputs
+def enter_datetime(
+  #str(input("Date you found specimen? "))
+  #str(input("Time of day you found specimen? "))
+  break
+)
+# Status Inputs
+def enter_status(
+  #bool(input("Is your specimen clearly dead (warning: do not kill it if it is alive or potentially alive right now)? "))
+  break
+)
+# Metric Inputs
+def enter_metric(
+  #float(input("Length of specimen (cm)? "))
+  #float(input("Weight of specimen (kg)? "))
+  break
+)
+# Phenotypic Inputs
+def enter_phenotypic(
+  #bool(input("Is your specimen winged? "))
+  #int(input("How many legs does you specimen have? "))
+  #str(input("What is the main body color of your specimen? "))
+  break
+)
+# Entomologist's Inputs (for experienced arthropodologists)
+def enter_expert(
+  #str(input("What is the expected life stage of your specimen? "))
+  #int(input("Look closely at your specimen. How many sets of compound eyes do they have? "))
+  #int(input("Look closely at your specimen. How many sets of simple eyes (ocelli) do they have? "))
+  break
+)
