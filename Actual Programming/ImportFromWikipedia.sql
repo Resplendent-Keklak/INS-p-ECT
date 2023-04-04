782ca383ed9806ba66ae35db4c99d23a0ffe9db2
Keklake-of-fire <107497590+Resplendent-Keklak@users.noreply.github.com>
2023-04-04 09:15:15 -0400

    Update ImportFromWikipedia.sql

--- Actual Programming/ImportFromWikipedia.sql
+++ Actual Programming/ImportFromWikipedia.sql
@@ -1,15 +1,13 @@
 CREATE TABLE mytable(
    id     INTEGER  NOT NULL PRIMARY KEY,
    common   VARCHAR(50) NOT NULL,
-   order VARCHAR(50) NOT NULL,
-   family VARCHAR(50) NOT NULL,
-   genus VARCHAR(50) NOT NULL,
-   species VARCHAR(50) NOT NULL,
+   taxorder VARCHAR(50) NOT NULL,
+   taxfamily VARCHAR(50) NOT NULL,
+   taxgenus VARCHAR(50) NOT NULL,
+   taxspecies VARCHAR(50) NOT NULL,
    discovery INTERVAL YEAR[(4)],
 );
-INSERT INTO mytable(id,common,order,family,genus,species,discovery) VALUES (1,'Johnson, Smith, and Jones Co.',345.33,'Pays on time');
-INSERT INTO mytable(id,common,order,family,genus,species,discovery) VALUES (2,'Johnson, Smith, and Jones Co.',345.33,'Pays on time');
-INSERT INTO mytable(id,common,order,family,genus,species,discovery) VALUES (3,'Johnson, Smith, and Jones Co.',345.33,'Pays on time');
-INSERT INTO mytable(id,common,order,family,genus,species,discovery) VALUES (4,'Johnson, Smith, and Jones Co.',345.33,'Pays on time');
-INSERT INTO mytable(id,common,order,family,genus,species,discovery) VALUES (5,'Johnson, Smith, and Jones Co.',345.33,'Pays on time');
-INSERT INTO mytable(id,common,order,family,genus,species,discovery) VALUES (6,'Johnson, Smith, and Jones Co.',345.33,'Pays on time');
+INSERT INTO mytable(id,common,taxorder,family,genus,species,discovery) VALUES (1,'Red Paper Wasp','Hymenoptera','N/A','Polistes','metricus');
+INSERT INTO mytable(id,common,taxorder,family,genus,species,discovery) VALUES (2,'Custom','CustomOrder','CustomFamily','CustomGenus','customspecies');
+INSERT INTO mytable(id,common,taxorder,family,genus,species,discovery) VALUES (3,'Australian Christmas Beetle','Coleoptera','N/A','N/A','N/A');
+INSERT INTO mytable(id,common,taxorder,family,genus,species,discovery) VALUES (4,'Spotted Lanternfly','Hemiptera','N/A','Lycorma','delicatula');
