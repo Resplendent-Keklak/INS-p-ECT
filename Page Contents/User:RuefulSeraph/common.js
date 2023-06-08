9a7e9a45ced948d88d1360fb1f4ba292d6a6f2ce
Keklake-of-fire <107497590+Resplendent-Keklak@users.noreply.github.com>
2023-06-08 19:29:38 -0400

    Separating older and newer articles by site
    * Create common.js
    
    Shows my admin's tool imports for when they are needed
    
    * Create main.html
    
    This is the way the admin account's user page looks...and I mean it's REALLY basic
    
    * Rename Page Contents/Arthropods.html to Page Contents/Weebly/Arthropods.html
    
    This is for the Weebly site
    
    * Rename Page Contents/Contact.html to Page Contents/Weebly/Contact.html
    
    This is for the Weebly site
    
    * Rename Page Contents/Databases.html to Page Contents/Weebly/Databases.html
    
    This is for the Weebly site
    
    * Rename Page Contents/Docs.html to Page Contents/Weebly/Docs.html
    
    This is for the Weebly site
    
    * Rename Page Contents/Weebly/Arthropods.html to Page Contents/Weebly/Weebly/Arthropods.html
    
    This is for the Weebly site
    
    * Rename Page Contents/Weebly/Weebly/Arthropods.html to Page Contents/Weebly/Arthropods.html
    
    Bruh
    
    * Rename Page Contents/Home.html to Page Contents/Weebly/Home.html
    
    This is for the Weebly site
    
    * Rename Page Contents/Search.html to Page Contents/Weebly/Search.html
    
    This is for the Weebly site
    
    * Rename Page Contents/Sitemap.html to Page Contents/Weebly/Sitemap.html
    
    This is for the Weebly site
    
    * Rename Page Contents/Software.html to Page Contents/Weebly/Software.html
    
    This is for the Weebly site
    
    * Rename Page Contents/Species-map.html to Page Contents/Weebly/Species-Map.html
    
    This is for the Weebly site +renamed to proper title
    
    * Rename Page Contents/Taxonomy.html to Page Contents/Weebly/Taxonomy.html
    
    This is for the Weebly site

---
+++ Page Contents/User:RuefulSeraph/common.js
@@ -0,0 +1,22 @@
+//Imported Scripts from Miraheze Developers Wiki
+  //AjaxDelete
+mw.loader.load("https://meta.miraheze.org/wiki/User:Collei/AjaxDelete.js?action=raw&ctype=text/javascript");
+  //JWB
+mw.loader.load('//en.wikipedia.org/w/index.php?title=User:Joeytje50/JWB.js/load.js&action=raw&ctype=text/javascript');
+    //config
+window.AjaxDeleteReasons = [
+  "Housekeeping",
+  "Unused",
+  "Vandalism",
+  "Broken redirect",
+];
+  //BackendInformation
+mw.loader.load("https://dev.miraheze.org/wiki/MediaWiki:BackendInformation.js?action=raw&ctype=text/javascript");
+  //FastDelete
+mw.loader.load("https://meta.miraheze.org/wiki/User:Collei/FastDelete.js?action=raw&ctype=text/javascript");
+  //MassDelete
+mw.loader.load("https://dev.miraheze.org/wiki/MediaWiki:MassDelete.js?action=raw&ctype=text/javascript");
+  //MultiUpload
+mw.loader.load("https://dev.miraheze.org/wiki/MediaWiki:MultiUpload.js?action=raw&ctype=text/javascript");
+  //Jquerymigrate-3.3.2.js
+mw.loader.load("https://dev.miraheze.org/wiki/MediaWiki:Jquerymigrate-3.3.2.js?action=raw&ctype=text/javascript");
